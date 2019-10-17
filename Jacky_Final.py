2ad99391c19179970eaabebabf07ad4d64d738de
junenite <simjacky1997@gmail.com>
2019-10-17 11:18:02 +0800

    Jacky Function
    Cleaned up stuff and added meaningful comments

--- Jacky_Final.py
+++ Jacky_Final.py
@@ -4,17 +4,11 @@ import matplotlib.pyplot as plt
 
 def sortByArea():
     """Sorts the major shopping malls, attractions and hotel carpark by their area"""
-    #matplotlib.pyplot as plt helps to plot stuff
-    # set font and plot size to be larger
-    #plt.rcParams.update({'font.size': 20, 'figure.figsize': (10, 8)})
+
     df = pd.read_csv('Datasets/Carpark Rates.csv', index_col='carpark')
     cat = df['category']
 
-    #.plot(kind='hist', title='Area');
     cat_dict = {}
-
-    #getting the results (ONLY NO. OF OCCURRENCE) but not the name per se
-    #cat_value = cat.value_counts()
 
     #This is to add BOTH THE OCCURRENCE and NAME into a dictionary so can call it and display
     for x in cat:
@@ -24,14 +18,6 @@ def sortByArea():
         else:
             cat_dict[x] = 1
 
-    #print cat_dict
-    #print cat_value
-
-    #Because cat_value only have the occurrence, dont have the header as x axis
-    #cat_value.plot(kind='hist', title='Area');
-    #plt.show()
-
-    #print cat_dict
     cat_name = list(cat_dict.keys())
     cat_v = list(cat_dict.values())
 
@@ -47,23 +33,16 @@ def sortByArea():
     plt.setp(labels, rotation=45, horizontalalignment='right')
 
     #setting the name
-    ax.set(xlabel="Number of carparks", ylabel="Area", title="Major Shopping Malls, Attractions and Hotels sorted by area")
+    ax.set(xlabel="Number of carparks", ylabel="Category/Area", title="Major Shopping Malls, Attractions and Hotels sorted by Category/Area")
     plt.show()
 
 
 def sortByCarparkType():
-    """Sorts the major shopping malls, attractions and hotel carpark by their area"""
-    #matplotlib.pyplot as plt helps to plot stuff
-    # set font and plot size to be larger
-    #plt.rcParams.update({'font.size': 20, 'figure.figsize': (10, 8)})
+    """Displays a piechart on the different types of carpark available in HDB Carparks"""
     df = pd.read_csv('Datasets/Hdb Carpark Information.csv')
     type = df['car_park_type']
 
-    #.plot(kind='hist', title='Area');
     type_dict = {}
-
-    #getting the results (ONLY NO. OF OCCURRENCE) but not the name per se
-    #cat_value = cat.value_counts()
 
     #This is to add BOTH THE OCCURRENCE and NAME into a dictionary so can call it and display
     for x in type:
@@ -74,6 +53,7 @@ def sortByCarparkType():
             type_dict[x] = 1
     newdict = {}
     otherNum = 0
+    #Copying the data into another dictionary, compiling those below a certain number of occurrence into 1 category
     for (key, value) in type_dict.items():
         if type_dict[key] < 50:
             otherNum += value
@@ -81,8 +61,10 @@ def sortByCarparkType():
         else:
             newdict.update({key: value})
 
+    #assigning the values into a list to be placed into the chart
     type_name = list(newdict.keys())
     type_v = list(newdict.values())
+    #explode is used to pull a specific data in the chart so that it stands out
     explode = [0,0,1]
 
     fig1, ax1 = plt.subplots()
@@ -100,18 +82,17 @@ def sortByCarparkType():
 
 
 def sortCarTypeLots():
-    """Sorts the major shopping malls, attractions and hotel carpark by their area"""
+    """Displays a piechart for the total number of lots for the different vehicle type from URA Carpark data set"""
     df = pd.read_csv('Datasets/URA Carpark Information By Type Of Lot.csv')
     lotType = df.loc[:,['type_of_lot','number_of_lots']]
-    #lotTypeValue = df['number_of_lots']
-    #print lotType
-    #print lotTypeValue
 
-    #.plot(kind='hist', title='Area');
     lotType_dict = {}
 
+    #This is to iterate through each and every rows in the data set
     for index,rows in lotType.iterrows():
+        #to check if the type of lot is already in the dictionary or not
         if rows["type_of_lot"] in lotType_dict:
+            #this is to retrieve SPECIFIC number of lots to add on to SPECIFIC type of lot
             lotType_dict[rows["type_of_lot"]] += rows["number_of_lots"]
         else:
             lotType_dict[rows["type_of_lot"]] = rows["number_of_lots"]
@@ -119,16 +100,14 @@ def sortCarTypeLots():
     #print(lotType_dict)
 
 
-    #getting the results (ONLY NO. OF OCCURRENCE) but not the name per se
-    #cat_value = cat.value_counts()
-    #This is to add BOTH THE OCCURRENCE and NAME into a dictionary so can call it and display
+    #assigning the values to be placed in a list
     lotType_name = list(lotType_dict.keys())
     lotType_v = list(lotType_dict.values())
-    #print lotType_dict
 
     fig1, ax1 = plt.subplots()
     #autopct is to show the percentage, based on the formatting selected
-    #therefore chosen formatting is to display in 2 d.p.
+    #For this part, because showing the number of lots has more value than the percentage
+    #i chose to show the value instead of percentage
     total = sum(lotType_v)
     ax1.pie(lotType_v, labels=lotType_name, autopct=lambda(p): '{:.0f}'.format(p * total / 100), startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
