05d8c6f6ccf5bf14795ccee58af55436f095bd71
Agentzhao <tan.hongzhao@dhs.sg>
2019-10-19 10:37:21 +0800

    updated to fit qikai's api data

--- functions (URA Carpark Fees).py
+++ functions (URA Carpark Fees).py
@@ -2,18 +2,17 @@ import os
 import pandas as pd
 
 #data processing into dataframe
-raw_data = pd.read_csv("Datasets/URA Carpark Fees.csv")
+raw_data = pd.read_csv("Datasets/carparkFees.csv")
 columns = raw_data.columns.tolist()
 df = pd.DataFrame(raw_data, columns = columns)
 
 def allows_vehicle(carpark_name, vehicle_type):
     '''Check if URA carpark allows for vehicle_type to park. Car, Motorcycle or Heavy Vechicle'''
-    ppCode = df.loc[df.loc[df['ppName'].str.contains(carpark_name)].index[0], 'ppCode']
+    ppCode = df.loc[df.loc[df['Carpark Name'].str.contains(carpark_name)].index[0], 'ppCode']
     for row in range(len(df)):
         if df.loc[row].ppCode == ppCode:
-            if df.loc[row].vehCat == vehicle_type:
+            if df.loc[row]['Lot Type'] == vehicle_type:
                 return True
     return False
 
-#allows_vehicle('AMOY STREET OFF STREET', 'Car')
-
+allows_vehicle('AMOY STREET OFF STREET', 'Car')
